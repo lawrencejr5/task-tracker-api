5b0ed25d85be4fa2198c0abaf7e7b436372c1bde
lawrencejr5 <oputalawrence@gmail.com>
2024-03-26 15:48:43 +0100

    basic api setup complete

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("task-tracker");
+const express = require("express");
+const app = express();
+
+const task_routes = require("./routes/tasks");
+
+const port = 5000;
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Testing Task Tracker Applcation");
+});
+
+app.use("/api/v1/tasks", task_routes);
+app.use(express.json());
+
+app.listen(port, console.log(`App listening on port ${port}...`));
